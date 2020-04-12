a9bc80671b8f47f91c268db671fa8faca38a3c1f
ah265@duke.edu <ah265@duke.edu>
2020-04-12 12:15:08 -0700

    listening to remote commands from AWS Shadow to turn listening on / off instead of using Raspberry PI switch. Simplified listening.

--- kitchen.py
+++ kitchen.py
@@ -1,78 +1,80 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, print_function
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
-from playsound import playsound
 from gpiozero import MotionSensor
-import gpio
-from datetime import datetime
 import time
-import logging
 import json
 import os
 
-# A random programmatic shadow client ID.
+
+# update global state to desired state sent from shadow
+def update_state(payload, responseStatus, token):
+    # process payload
+    payload = json.loads(payload)
+
+    global state
+    state = payload['state']['monitor']
+    print('Updated listening state to ' + state)
+
+    # let shadow know you've updated
+    delta = json.dumps(payload['state'])
+    newPayload = '{"state":{"reported":' + delta + '}}'
+    myDeviceShadow.shadowUpdate(newPayload, my_shadow_update_callback, 5)
+
+
+# Configure shadow client with localy stored creds, associate to thing
 SHADOW_CLIENT = "myShadowClient"
-
-# The unique hostname that &IoT; generated for
-# this device.
 HOST_NAME = "a1r58egst5kiuu-ats.iot.us-west-2.amazonaws.com"
-
-# The relative path to the correct root CA file for &IoT;,
-# which you have already saved onto this device.
 ROOT_CA = "root-CA.crt"
-
-# The relative path to your private key file that
-# &IoT; generated for this device, which you
-# have already saved onto this device.
 PRIVATE_KEY = "056e1e33f1-private.pem.key"
-# The relative path to your certificate file that 
-# &IoT; generated for this device, which you 
-# have already saved onto this device.
 CERT_FILE = "056e1e33f1-certificate.pem.crt"
-
-# A programmatic shadow handler name prefix.
 SHADOW_HANDLER = "Pi"
 
-def myShadowUpdateCallback(payload, responseStatus, token):
- print('Loading function')
- print('UPDATE: $aws/things/' + SHADOW_HANDLER +
-    '/shadow/update/#')
- print("payload = " + payload)
- print("responseStatus = " + responseStatus)
- print("token = " + token)
+
+# Callback function for AWS IOT
+def my_shadow_update_callback(payload, responseStatus, token):
+    print('Loading function')
+    print('UPDATE: $aws/things/' + SHADOW_HANDLER + '/shadow/update/#')
+    print("payload = " + payload)
+    print("responseStatus = " + responseStatus)
+    print("token = " + token)
+
 
 # Create, configure, and connect a shadow client.
+print('Starting up AWS IOT shadow...')
 myShadowClient = AWSIoTMQTTShadowClient(SHADOW_CLIENT)
 myShadowClient.configureEndpoint(HOST_NAME, 8883)
-myShadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY,
-  CERT_FILE)
+myShadowClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERT_FILE)
 myShadowClient.configureConnectDisconnectTimeout(10)
 myShadowClient.configureMQTTOperationTimeout(5)
 myShadowClient.connect()
 # Create a programmatic representation of the shadow.
-myDeviceShadow = myShadowClient.createShadowHandlerWithName(
-  SHADOW_HANDLER, True)
+myDeviceShadow = myShadowClient.createShadowHandlerWithName(SHADOW_HANDLER, True)
+print('Connected to AWS IOT with locally loaded shadow!')
 
-# pickup sensor on RaspberryPi
-def activateGPIO():
-    pir = MotionSensor(4)
-    print('PIR sensor using ' + str(pir.pin))
-    switch_pin = 27
-    return(switch_pin,pir)
+# Listen for deltas from AWS IOT shadow in separate thread. Call update_state when triggered.
+myDeviceShadow.shadowRegisterDeltaCallback(update_state)
 
-if __name__== "__main__":
-    print('Starting up...')
-    switch_pin,pir = activateGPIO()
-    while True:
-        if(gpio.input(switch_pin) == 1):
-            print("Switch ON!")
-            print('Waiting for motion...')
-            pir.wait_for_motion()
-            print(pir.is_active)
-            #if switch was disconnected while waiting for motion
-            if(gpio.input(switch_pin) == 1):
-                print('////////////////////////Motion Detected, logging the sucker!')
-                os.system("omxplayer -o local airhorn-siren.mp3")
-                myDeviceShadow.shadowUpdate('{"state":{"reported":{"motion":"detected"}}}',myShadowUpdateCallback,5)
+# pickup motion sensor on RaspberryPi
+pir = MotionSensor(4)
+print('PIR sensor detected, using pin ' + str(pir.pin))
+
+# listen for motion by default
+state = "enabled"
+
+while True:
+    if state == "enabled":
+        print("Switch ON!")
+        print('Waiting for motion...')
+        pir.wait_for_motion()
+        # if switch was disconnected while waiting for motion
+        if state == "enabled":
+            print('////////////////////////Motion Detected, logging the sucker!////////////////////////')
+            os.system("omxplayer -o local airhorn-siren.mp3")
+            myDeviceShadow.shadowUpdate('{"state":{"reported":{"motion":"detected"}}}', my_shadow_update_callback, 5)
         else:
-            print('Switch off :(')
+            print("switch turned of before ")
+    else:
+        print('Switch OFF :(')
+
+    time.sleep(15)
